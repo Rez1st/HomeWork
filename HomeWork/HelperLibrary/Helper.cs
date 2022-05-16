7582931186da690616fbe1c9e7e9210f316cb641
Tymur Rustamov <gtimzua@gmail.com>
2022-05-16 20:15:45 +0300

    added helper

---
+++ HomeWork/HelperLibrary/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelperLibrary
+{
+    public static class Helper
+    {
+        public static void PrintWithColor(string message, ConsoleColor color, bool newLine)
+        {
+            var s = newLine ? "\n" : "";
+            Console.ForegroundColor = color;
+            Console.Write($"{message} {s}");
+            Console.ResetColor();
+        }
+    }
+}
--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -1,10 +1,13 @@
-﻿
+﻿using System;
+using HelperLibrary;
+
 namespace HomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Helper.PrintWithColor("Hello World", ConsoleColor.DarkRed, true);
         }
     }
 }
